2710f8ea2331856630a3453c69b50a1dc31021c2
Pushkar <pushkarpandey2196@gmail.com>
2022-01-01 21:38:35 +0530

    Time: 2 ms (41.00%), Space: 52.6 MB (7.75%) - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while (j < nums.length) {
+            if(nums[j] == 0) {
+                j++;
+                continue;
+            }
+            nums[i] = nums[j];
+            i++;
+            j++;
+        }
+        while (i < nums.length) {
+            nums[i++] = 0;
+        }
+    }
+}
