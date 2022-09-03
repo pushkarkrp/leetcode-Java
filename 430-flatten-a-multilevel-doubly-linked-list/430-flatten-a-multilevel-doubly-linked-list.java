5b5d9e2262f82924284d4f6c56f25205fc4a7fc3
Pushkar <pushkarpandey2196@gmail.com>
2022-09-02 18:11:47 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (83.68%) - LeetHub

---
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    Node tail = null;
+public Node flatten(Node head) {
+    if(head == null) return null;
+    
+    head.prev = tail;
+    tail = head;
+    
+    Node nextNode = head.next;
+    
+    head.next = flatten(head.child);
+    head.child = null;
+    
+    tail.next = flatten(nextNode);
+    
+    return head;
+}
+}
