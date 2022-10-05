13628d9760fd57bc0d014c6871d42c4640603bab
Pushkar <pushkarpandey2196@gmail.com>
2022-10-04 20:08:15 -0400

    Time: 2 ms (20.26%), Space: 41.9 MB (43.10%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.java
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.java
@@ -8,14 +8,22 @@
 
 class Solution {
     public int countShips(Sea sea, int[] topRight, int[] bottomLeft) {
-        if (!sea.hasShips(topRight, bottomLeft)) return 0;
-        if (topRight[0] == bottomLeft[0] && topRight[1] == bottomLeft[1]) return 1;
-        
-        int midX = (topRight[0] + bottomLeft[0])/2;
-        int midY = (topRight[1] + bottomLeft[1])/2;
-        return countShips(sea, new int[]{midX, midY}, bottomLeft) +
-            countShips(sea, topRight, new int[]{midX+1, midY+1}) +
-            countShips(sea, new int[]{midX, topRight[1]}, new int[]{bottomLeft[0], midY+1}) +
-            countShips(sea, new int[]{topRight[0], midY}, new int[]{midX+1, bottomLeft[1]});
+        if (bottomLeft[0] > topRight[0] || bottomLeft[1] > topRight[1]) {
+            return 0;
+        }
+        if (!sea.hasShips(topRight, bottomLeft)) {
+            return 0;
+        }
+        if (bottomLeft[0] == topRight[0] && bottomLeft[1] == topRight[1]) {
+            return 1;
+        }
+        int result = 0;
+        int midx = (topRight[0] + bottomLeft[0]) / 2;
+        int midy = (topRight[1] + bottomLeft[1]) / 2;
+        result += countShips(sea, new int[]{midx, midy}, new int[]{bottomLeft[0],bottomLeft[1]});
+        result += countShips(sea, new int[]{topRight[0], midy}, new int[]{midx+1,bottomLeft[1]});
+        result += countShips(sea, new int[]{midx, topRight[1]}, new int[]{bottomLeft[0],midy+1});
+        result += countShips(sea, new int[]{topRight[0], topRight[1]}, new int[]{midx+1,midy+1});
+        return result;
     }
 }
